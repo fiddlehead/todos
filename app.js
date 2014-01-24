4c1f24bd42c6d21500cdafd85dcf328c5e9c8ccd
jpedelen <jpedelen@yahoo.com>
2014-01-24 11:19:54 -0600

    app progress

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var routes=require('./routes');
+var app = express();
+
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(app.router);
+
+app.get('/', routes.index);
+app.get('/todos', routes.todos);
+app.post('/todos', routes.submit);
+app.get('/todos/:id', routes.select);
+app.post('/todos/:id', routes.change);
+
+app.listen(4242);
---
+++ routes.js
@@ -0,0 +1,41 @@
+var uuid = require('node-uuid');
+var jf = require('jsonfile');
+var file = 'Json/data.json';
+entries = jf.readFileSync(file);
+function find(id,array){
+    for(var i=0; i<array.length; i++){
+        if(array[i].id==id)
+            return array[i];
+    }
+};
+
+exports.index=function(req,res){
+    res.sendfile("views/index.html");
+};
+
+exports.todos=function(req,res){
+    res.sendfile(file);
+};
+
+exports.submit=function(req,res){
+    var new_todo= {
+    "name":req.body.item,
+    "isDone":false,
+    "id":uuid.v4()
+    };
+    entries.push(new_todo);
+    jf.writeFileSync(file, entries);
+    res.redirect("/");
+};
+
+exports.select=function(req,res){
+    var found= find(req.params.id, entries);
+    res.send(found);
+};
+
+exports.change=function(req,res){
+    var found= find(req.params.id, entries);
+    found.isDone=true;
+    jf.writeFileSync(file, entries);
+
+};
